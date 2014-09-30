f9879e77c3d97ca3d3db26aa307e3ad1994e03ec
electricessence <oren@electrified.net>
2014-09-30 14:40:39 -0700

    Updated IGrouping for groupBy to Grouping.

--- build/System.Linq.d.ts
+++ build/System.Linq.d.ts
@@ -106,7 +106,7 @@ declare module System.Linq {
         public union<TCompare>(second: Collections.IArray<T>, compareSelector?: Selector<T, TCompare>): Enumerable<T>;
         public orderBy<TKey>(keySelector?: Selector<T, TKey>): OrderedEnumerable<T>;
         public orderByDescending<TKey>(keySelector?: Selector<T, TKey>): OrderedEnumerable<T>;
-        public groupBy<TKey, TElement, TCompare>(keySelector: Selector<T, TKey>, elementSelector?: Selector<T, TElement>, compareSelector?: Selector<TKey, TCompare>): Enumerable<IGrouping<TKey, TElement>>;
+        public groupBy<TKey, TElement, TCompare>(keySelector: Selector<T, TKey>, elementSelector?: Selector<T, TElement>, compareSelector?: Selector<TKey, TCompare>): Enumerable<Grouping<TKey, TElement>>;
         public partitionBy<TKey, TElement, TCompare>(keySelector: Selector<T, TKey>, elementSelector?: Selector<T, TElement>, resultSelector?: (key: TKey, element: TElement[]) => IGrouping<TKey, TElement>, compareSelector?: Selector<TKey, TCompare>): Enumerable<IGrouping<TKey, TElement>>;
         public buffer(size: number): Collections.IEnumerable<T[]>;
         public aggregate(func: (a: T, b: T) => T, seed?: T): T;
