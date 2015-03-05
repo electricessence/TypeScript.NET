875088c52ffb9b9fb8c96d1b2cc1a5728db7b951
Oren Ferrari <oren.ferrari@ge.com>
2015-03-05 15:30:15 -0800

    Added AMD versions to gulp! It WORKS! :) Had to use gulp-wrap-js instead of gulp-r. gulp-r overcomplicated the steps when all that was required was a simple wrap. gulp-r also bloated the files to more than double their uglified size. I left the amd versions in thier own folder as a sibling to build so that they can behave the same.

--- System/gulpfile.js
+++ System/gulpfile.js
@@ -1,84 +1,12 @@
 var gulp = require('gulp');
 
-// TypeScript Compilation..
-(function(namespace) {
+// Initialize the tasks array.
+var tasks = [];
 
-	const PARENT = '../', TS = '.ts', JS = '.js', MIN = '.min', AMD = '.amd';
-	const BUILD_FOLDER = PARENT + 'build/';
-	const SOURCE_ROOT = PARENT + namespace;
+// Setup the function to append the task names to the array.
+function addTask(task,namespace,dependencies) { tasks = tasks.concat(require('../gulp-tasks/' + task)(gulp)(namespace,dependencies)); }
 
-	const TASK_COMPILE = namespace + ' compile';
-	const TASK_COMPRESS = TASK_COMPILE + MIN;
-	const TASK_AMD = TASK_COMPILE + AMD;
+// Add the task to gulp.
+addTask('compile-namespace','System');
 
-	const JS_OUT = namespace + JS;
-
-	var srcPaths = ['**/*' + TS];
-
-	['Globalization/TimeSpanFormat' + TS]
-		.forEach(
-		function(f) {
-			srcPaths.push('!' + f);
-		});
-
-	var typescript = require('gulp-tsc');
-	gulp.task(
-		TASK_COMPILE, function() {
-			return gulp.src(srcPaths) // Ignore
-				.pipe(
-				typescript(
-					{
-						out: JS_OUT,
-						target: 'es5',
-						sourcemap: true,
-						sourceRoot: SOURCE_ROOT,
-						removeComments: true,
-						noImplicitAny: true,
-						declaration: true
-					}))
-				.pipe(gulp.dest(BUILD_FOLDER));
-		});
-
-	var rename = require('gulp-rename');
-	var sourcemaps = require('gulp-sourcemaps');
-
-	var rjs = require('gulp-r');
-	gulp.task(
-		TASK_AMD, [TASK_COMPILE], function() {
-			return gulp.src([BUILD_FOLDER + namespace + JS])
-				.pipe(sourcemaps.init({loadMaps: true}))
-				.pipe(
-				rjs(
-					{
-						baseUrl: BUILD_FOLDER,
-						shim: {
-							"System": {
-								deps: [],
-								exports: "System"
-							}
-						}
-					}))
-				.pipe(rename(namespace + AMD + JS))
-				.pipe(sourcemaps.write(BUILD_FOLDER, {includeContent: false}))
-				.pipe(gulp.dest(BUILD_FOLDER));
-		});
-
-
-	var uglify = require('gulp-uglify');
-	gulp.task(
-		TASK_COMPRESS, [TASK_COMPILE], function() { // Have 'TASK_COMPILE' as a dependency to ensure constraints.
-			return gulp.src(BUILD_FOLDER + JS_OUT)
-				.pipe(sourcemaps.init({loadMaps: true}))
-				.pipe(uglify())
-				.pipe(rename(namespace + MIN + JS))
-				.pipe(sourcemaps.write(BUILD_FOLDER, {includeContent: false}))
-				.pipe(gulp.dest(BUILD_FOLDER));
-		});
-
-
-	gulp.task(
-		'default', [TASK_COMPILE, TASK_COMPRESS, TASK_AMD], function() {
-		});
-
-
-})('System');
+gulp.task( 'default', tasks );
--- build/tests/amd/load-test.js
+++
@@ -1,5 +0,0 @@
-requirejs(
-	// First verify that requirejs correctly pulls in System as a dependency.
-	['System', 'System.Linq'], function(System, Linq) {
-		alert(('Collections' in System && 'Enumerable' in Linq) ? 'Success.' : 'Fail.');
-	});
