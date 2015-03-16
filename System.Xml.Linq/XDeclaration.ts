f0de4d9b740aebc406fd8caf10433a864a74ccb6
Alex <alex@amastermind.com>
2015-03-16 12:57:57 +0100

    implemented XDeclaration

--- System.Xml.Linq/XDeclaration.ts
+++ System.Xml.Linq/XDeclaration.ts
@@ -10,9 +10,56 @@
 module System.Xml.Linq
 {
 	export class XDeclaration
-	{
-		constructor()
-		{
-		}
+    {
+        private _version: string;
+        private _encoding: string;
+        private _standalone: string;
+        
+        constructor(version: string, encoding: string, standalone: string)
+        {
+            this._version = version;
+            this._encoding = encoding;
+            this._standalone = standalone;
+        }
+
+        /* TODO
+        public XDeclaration(XDeclaration other) {
+            if (other == null) throw new ArgumentNullException("other");
+            version = other.version;
+            encoding = other.encoding;
+            standalone = other.standalone;
+        }
+        */
+
+        get version(): string { return this._version; }
+        set version(value: string) { this._version = value; }
+
+        get encoding(): string { return this._encoding; }
+        set encoding(value: string) { this._encoding = value; }
+
+        get standalone(): string { return this._standalone; }
+        set standalone(value: string) { this._standalone = value; }
+
+        public toString(): string
+        {
+            var s =  "<?xml";
+            if (!System.isNullOrUndefined(this.version)) {
+                s += " version=\"";
+                s += this.version;
+                s += "\"";
+            }
+            if (!System.isNullOrUndefined(this.encoding)) {
+                s += " encoding=\"";
+                s += this.encoding;
+                s += "\"";
+            }
+            if (!System.isNullOrUndefined(this.standalone)) {
+                s += " standalone=\"";
+                s += this.standalone;
+                s += "\"";
+            }
+            s += "?>";
+            return s;
+        }
 	}
 }
