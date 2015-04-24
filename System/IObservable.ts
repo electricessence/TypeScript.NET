9287528e13890dbee240f22960dd7412715168ab
electricessence <electricessence@live.com>
2015-04-23 18:04:28 -0700

    Added IObservable and IObserver interfaces.

---
+++ System/IObservable.ts
@@ -0,0 +1,17 @@
+﻿/*
+* @author electricessence / https://github.com/electricessence/
+* Based upon .NET source.
+* Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE
+*/
+
+module System
+{
+
+	// http://referencesource.microsoft.com/#mscorlib/system/iobservable.cs
+	// https://msdn.microsoft.com/en-us/library/dd990377.aspx
+	export interface IObservable<T> // <out T>
+	{
+		subscribe(observer: IObserver<T>): IDisposable;
+  	}
+
+}
---
+++ System/IObserver.ts
@@ -0,0 +1,18 @@
+﻿/*
+* @author electricessence / https://github.com/electricessence/
+* Based upon .NET source.
+* Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE
+*/
+
+module System
+{
+
+	// http://referencesource.microsoft.com/#mscorlib/system/iobserver.cs
+	export interface IObserver<T> // <in T>
+	{
+        onNext(value: T): void;
+        onError(error: Error): void;
+        onCompleted():void;
+	}
+
+} 
