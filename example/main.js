8908236d26ad4e8825ef5d46abf93f22f606d0d5
electricessence <oren@electrified.net>
2015-09-16 20:32:38 -0700

    Updated example using a main module.

--- example/main.js
+++ example/main.js
@@ -1,15 +1,5 @@
-requirejs.config(
-	{
-		baseUrl: '../'
-	}
-);
-require(
-	[
-		'source/System/Collections/Arrays/Utility'
-	],
-
-	function(ArrayUtil) {
-		var a = ArrayUtil.initialize(50);
-		alert(a.length);
-	}
-);
+define(["require", "exports", '../source/System/Collections/Arrays/Utility'], function (require, exports, ArrayUtility) {
+    var a = ArrayUtility.initialize(50);
+    alert(a.length);
+});
+//# sourceMappingURL=main.js.map
---
+++ example/main.ts
@@ -0,0 +1,5 @@
+import ArrayUtility = require('../source/System/Collections/Arrays/Utility');
+
+var a = ArrayUtility.initialize(50);
+alert(a.length);
+
