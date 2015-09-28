61898afb068417428b8bedca7428ee0483d18506
electricessence <oren@electrified.net>
2015-09-28 16:58:10 -0700

    Updated JS render.

--- source/System/Collections/Dictionaries/Dictionary.js
+++ source/System/Collections/Dictionaries/Dictionary.js
@@ -6,8 +6,7 @@
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 define(["require", "exports", '../../Compare', '../../Types', '../../Functions', './DictionaryAbstractBase', '../Enumeration/EnumeratorBase'], function (require, exports, Values, Types, Functions, DictionaryAbstractBase, EnumeratorBase) {
     var HashEntry = (function () {
