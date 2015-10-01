b223a28d49cbeb0ab9824fe7eaf71fb590caedde
electricessence <oren@electrified.net>
2015-09-30 23:52:37 -0700

    Simple test.

---
+++ tests/simple.ts
@@ -0,0 +1,6 @@
+import ArrayUtility = require('../source/System/Collections/Array/Utility');
+import TextUtility = require( '../source/System/Text/Utility');
+
+var a = ArrayUtility.initialize(50);
+alert(TextUtility.supplant('Array Initialized to {i}.', {i: a.length}));
+
