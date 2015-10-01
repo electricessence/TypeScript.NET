37686a749ff0812d0c2c72cb15fcc9b835f93d82
electricessence <oren@electrified.net>
2015-10-01 00:18:09 -0700

    Added missing files.

---
+++ tests/simple.js
@@ -0,0 +1,5 @@
+define(["require", "exports", '../source/System/Collections/Array/Utility', '../source/System/Text/Utility'], function (require, exports, ArrayUtility, TextUtility) {
+    var a = ArrayUtility.initialize(50);
+    alert(TextUtility.supplant('Array Initialized to {i}.', { i: a.length }));
+});
+//# sourceMappingURL=simple.js.map
