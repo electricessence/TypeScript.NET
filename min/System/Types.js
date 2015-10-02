414371412b77a9810eef00eec5a1ececf5ff24af
electricessence <oren@electrified.net>
2015-10-01 23:32:12 -0700

    Updated minified versions.

--- min/System/Types.js
+++ min/System/Types.js
@@ -2,5 +2,5 @@
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
  */
-"use strict";define(["require","exports","./Functions"],function(n,e,t){var r;return function(n){function e(e){return typeof e===n.Boolean}function r(e){return typeof e===n.Number}function o(e){return typeof e===n.Number&&isNaN(e)}function u(e){return typeof e===n.String}function i(e){return typeof e===n.Function}n.Boolean=typeof!0,n.Number="number",n.String="string",n.Object=typeof{},n.Null="object",n.Undefined="undefined",n.Function=typeof t.Blank,n.isBoolean=e,n.isNumber=r,n.isTrueNaN=o,n.isString=u,n.isFunction=i}(r||(r={})),Object.freeze(r),r});
+define(["require","exports","./Functions"],function(n,e,t){var o;return function(n){function e(e){return typeof e===n.Boolean}function o(e){return typeof e===n.Number}function r(e){return typeof e===n.Number&&isNaN(e)}function u(e){return typeof e===n.String}function i(e){return typeof e===n.Function}n.Boolean=typeof!0,n.Number="number",n.String="string",n.Object=typeof{},n.Null="object",n.Undefined="undefined",n.Function=typeof t.Blank,n.isBoolean=e,n.isNumber=o,n.isTrueNaN=r,n.isString=u,n.isFunction=i}(o||(o={})),Object.freeze(o),o});
 //# sourceMappingURL=Types.js.map
--- min/System/Validation/ValidationResult.js
+++ min/System/Validation/ValidationResult.js
@@ -2,5 +2,5 @@
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
  */
-define(["require","exports"],function(e,n){var i=new t(!0),t=function(){function e(e,n,i){void 0===e&&(e=!1),void 0===n&&(n=null),void 0===i&&(i=null),this.isValid=e,this.message=n,this.data=i,Object.freeze(this)}return Object.defineProperty(e,"valid",{get:function(){return i},enumerable:!0,configurable:!0}),e.invalid=function(n,i){return void 0===i&&(i=null),new e(!1,n,i)},e}();return t});
+define(["require","exports"],function(e,i){var n=new t(!0),t=function(){function e(e,i,n){void 0===e&&(e=!1),void 0===i&&(i=null),void 0===n&&(n=null),this.isValid=e,this.message=i,this.data=n,Object.freeze(this)}return e.prototype.equals=function(e){var i=this;return i.isValid===e.isValid&&i.message==i.message&&i.data==i.data},Object.defineProperty(e,"valid",{get:function(){return n},enumerable:!0,configurable:!0}),e.invalid=function(i,n){return void 0===n&&(n=null),new e(!1,i,n)},e}();return t});
 //# sourceMappingURL=ValidationResult.js.map
