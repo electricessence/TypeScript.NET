d3adc729c042384759231abba339ac0f4b5007d8
electricessence <oren@electrified.net>
2015-10-04 19:33:02 -0700

    Significantly improved typedoc gulp task. Overall gulp task improvements.

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,15 @@
 var gulp = require('gulp');
 var del = require('del');
 var replace = require('gulp-replace');
+var EVENT_END = 'end';
 
 
 const DOCS = './documentation';
 
-function clearGulpTscTemp() {
-	//del(['./gulp-tsc-tmp-*']);
-	//del(['./source/.gulp-tsc-tmp-*']);
-}
-
 gulp.task(
 	// This renders the same output as WebStorm's configuration.
-	'typescript', function() {
+	'typescript', function()
+	{
 
 		var options = {
 			tscPath: './node_modules/typescript/bin/tsc',
@@ -27,20 +24,17 @@ gulp.task(
 		};
 
 		// In order to mirror WebStorm's compiler option, gulp-tsc is used.
-		var stream = gulp
-			    .src(['./source/**/*.ts'])
-			    .pipe(require('gulp-tsc')(options))
-			    .pipe(gulp.dest('./source'))
+		return gulp
+			.src(['./source/**/*.ts'])
+			.pipe(require('gulp-tsc')(options))
+			.pipe(gulp.dest('./source'))
 			;
-
-		clearGulpTscTemp();
-
-		return stream;
 	}
 );
 
 gulp.task(
-	'typescript.min', ['typescript'], function() {
+	'typescript.min', function()
+	{
 
 		del(['./min/**/*']);
 
@@ -62,7 +56,7 @@ gulp.task(
 		};
 
 
-		var stream = gulp
+		return gulp
 			.src(['./source/**/*.ts'])
 			.pipe(sourcemaps.init())
 			.pipe(require('gulp-typescript')({
@@ -75,69 +69,76 @@ gulp.task(
 			.pipe(sourcemaps.write('.', sourceMapOptions))
 			.pipe(gulp.dest('./min'));
 
-		clearGulpTscTemp();
-
-		return stream;
-
 	}
 );
 
 
 gulp.task(
-	'typedoc', ['typescript', 'typescript.min'], function() {
+	'typedoc', function(done) {
 
-		var typedoc = require('gulp-typedoc');
+		var typedoc = require('gulp-typedoc'),
+		    rename  = require('gulp-rename');
 
-		var stream = gulp
-			.src(['source'])
-			.pipe(
-			typedoc(
-				{
-					name: 'TypeScript.NET',
-					out: './documentation',
+		var typedocOptions = {
+			name: 'TypeScript.NET',
+			out: './documentation',
 
-					module: 'amd',
-					target: 'es5',
+			module: 'amd',
+			target: 'es5',
 
-					includeDeclarations: true,
-					ignoreCompilerErrors: false,
-					version: true
-				}
-			)
-		);
+			includeDeclarations: true,
+			ignoreCompilerErrors: false,
+			version: true
+		};
 
-
-		// Fix for issue with search that places a [BACK-SLASH] instead of a [SLASH].
-		const SEARCH_FOLDER = DOCS+'/assets/js';
+		// Step 1: Render type-docs..
+		console.log("TypeDocs: rendering");
 		gulp
-			.src([SEARCH_FOLDER + '/search.js'])
-			.pipe(replace('\\\\','/'))
-			.pipe(replace('/_','/'))
-			.pipe(gulp.dest(SEARCH_FOLDER));
+			.src('source')
+			.pipe(typedoc(typedocOptions))
+			.on(EVENT_END, function() {
 
-		return stream;
+				// Step 2-A: Fix for issue with search that places a [BACK-SLASH] instead of a [SLASH].
+				console.log("TypeDocs: applying fixes");
+				const SEARCH_FOLDER = DOCS + '/assets/js';
+				gulp
+					.src(SEARCH_FOLDER + '/search.js')
+					.pipe(replace('\\\\', '/'))
+					.pipe(replace('/_', '/'))
+					.pipe(gulp.dest(SEARCH_FOLDER));
+
+				// Step 2-B: Rename (rewrite) html files.
+				const PREFIXED_DOCS = DOCS + '/**/_*.html';
+				gulp.src(PREFIXED_DOCS)
+					.pipe(rename(
+						function(path) {
+							path.basename = path.basename.replace(/^_/, '');
+						}))
+					.pipe(gulp.dest(DOCS))
+					.on(EVENT_END, function() {
+						// Step 3: Delete all old underscored html files.
+						del.sync([PREFIXED_DOCS], function() {
+
+							// Step 4: Fix for issue where type-docs insert an [UNDERSCORE] in front of all .html files.
+							gulp
+								.src(DOCS + '/**/*.html')
+								.pipe(replace('/_', '/'))
+								.pipe(replace(' href="_', ' href="'))
+								.pipe(gulp.dest(DOCS))
+								.on(EVENT_END, function(){
+									console.log("TypeDocs: fixes complete");
+									done();
+								});
+
+						});
+					});
+
+			});
+
+
 
 	}
 );
-
-gulp.task('typedoc-fix',[],function(){
-
-	var rename = require('gulp-rename');
-
-	// Fix for issue where type-docs insert an [UNDERSCORE] in front of all .html files.
-	return gulp
-		.src([DOCS+'/**/*.html'])
-		.pipe(replace('/_','/'))
-		.pipe(replace(' href="_',' href="'))
-		.pipe(rename(function (path) {
-			path.basename = path.basename.replace(/^_/,'');
-		}))
-		.pipe(gulp.dest(DOCS));
-});
-
-gulp.task('typedoc-fix-cleanup',[],function(){
-	del([DOCS+'/**/_*.html']);
-});
 
 gulp.task('default', [
 	'typescript',
