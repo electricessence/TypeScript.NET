d22b75017b6a7f7a064db5eb12270fbca107fe4f
electricessence <oren@electrified.net>
2015-10-11 00:22:24 -0700

    Added and improved uri tests.

--- tests/Uri/_all.js
+++ tests/Uri/_all.js
@@ -8,7 +8,7 @@ define(["require", "exports", '../../source/System/Uri/Uri', "QUnit"], function 
             host: 'domain.com',
             port: 1234,
             path: '/tree/node/index.html',
-            query: '?param=value',
+            query: '?param=hello%20there&flag=false&blah',
             fragment: '#home'
         };
         var validUrl = ''
@@ -20,11 +20,14 @@ define(["require", "exports", '../../source/System/Uri/Uri', "QUnit"], function 
             + validUri.query
             + validUri.fragment;
         QUnit.test("Valid Uri", function (assert) {
-            assert.equal(Uri.toString(validUri), validUrl, "Uri does not match source values.");
+            assert.equal(Uri.toString(validUri), validUrl, "Uri.toString(uri) must match source values.");
             var uri = Uri.from(validUri);
-            assert.equal(uri.toString(), validUrl, "Uri does not match source values.");
-            assert.equal(uri.absoluteUri, validUrl, "Uri does not match source values.");
-            assert.equal(uri.pathAndQuery, uri.path + uri.query, "Uri path and query do not equal expected.");
+            assert.equal(uri.toString(), validUrl, "Uri.toString() must match source values.");
+            assert.equal(uri.absoluteUri, validUrl, "Uri.absoluteUri must match source values.");
+            assert.equal(uri.pathAndQuery, uri.path + uri.query, "Uri path and query must equal expected.");
+            assert.equal(uri.queryParams['param'], "hello there", "Uri must decode the query params correctly.");
+            assert.equal(uri.queryParams['flag'], false, "Uri must parse and deserialize the query params correctly.");
+            assert.equal(uri.queryParams['blah'], undefined, "Uri must ignore invalid query params.");
         });
         QUnit.test("Invalid scheme", function (assert) {
             assert.throws(function () {
--- tests/Uri/_all.ts
+++ tests/Uri/_all.ts
@@ -13,7 +13,7 @@ function run()
 		host:'domain.com',
 		port:1234,
 		path:'/tree/node/index.html',
-		query:'?param=value',
+		query:'?param=hello%20there&flag=false&blah',
 		fragment:'#home'
 	};
 	var validUrl:string = ''
@@ -33,25 +33,40 @@ function run()
 		assert.equal(
 			Uri.toString(validUri),
 			validUrl,
-			"Uri does not match source values.");
+			"Uri.toString(uri) must match source values.");
 
 
 		var uri = Uri.from(validUri);
 		assert.equal(
 			uri.toString(),
 			validUrl,
-			"Uri does not match source values.");
+			"Uri.toString() must match source values.");
 
 		assert.equal(
 			uri.absoluteUri,
 			validUrl,
-			"Uri does not match source values.");
+			"Uri.absoluteUri must match source values.");
 
 		assert.equal(
 			uri.pathAndQuery,
 			uri.path+uri.query,
-			"Uri path and query do not equal expected."
-		)
+			"Uri path and query must equal expected.");
+
+		assert.equal(
+			uri.queryParams['param'],
+			"hello there",
+			"Uri must decode the query params correctly.");
+
+		assert.equal(
+			uri.queryParams['flag'],
+			false,
+			"Uri must parse and deserialize the query params correctly.");
+
+		assert.equal(
+			uri.queryParams['blah'],
+			undefined,
+			"Uri must ignore invalid query params.");
+
 
 	});
 
