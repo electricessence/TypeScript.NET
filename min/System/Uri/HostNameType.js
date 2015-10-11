cfb0fff5261bfdb26ced149f8c7451ce6bd9b481
electricessence <oren@electrified.net>
2015-10-11 00:09:47 -0700

    Added missing files.  All related to new Uri class.

---
+++ min/System/Uri/HostNameType.js
@@ -0,0 +1,7 @@
+/*
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
+ * Based on: https://msdn.microsoft.com/en-us/library/system.UriHostNameType%28v=vs.110%29.aspx
+ */
+define(["require","exports"],function(n,e){var r;return function(n){n[n.Basic=0]="Basic",n[n.DNS=1]="DNS",n[n.IPv4=2]="IPv4",n[n.IPv6=3]="IPv6",n[n.Unknown=4]="Unknown"}(r||(r={})),Object.freeze(r),r});
+//# sourceMappingURL=HostNameType.js.map
