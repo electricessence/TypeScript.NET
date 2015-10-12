873d2047647c9ca4192242159fccb7499009eaa5
electricessence <oren@electrified.net>
2015-10-11 19:01:30 -0700

    Updated min version.

--- min/System/Types.js
+++ min/System/Types.js
@@ -2,5 +2,5 @@
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
  */
-define(["require","exports"],function(n,e){function t(n){return typeof n===f}function i(n,e){return void 0===e&&(e=!0),typeof n===N&&(e||!isNaN(n))}function o(n){return typeof n===N&&isNaN(n)}function r(n){return typeof n===p}function u(n){return typeof n===a}var f=typeof!0,N="number",p="string",s=typeof{},c="object",y="undefined",a=typeof u;e.BOOLEAN=f,e.NUMBER=N,e.STRING=p,e.OBJECT=s,e.NULL=c,e.UNDEFINED=y,e.FUNCTION=a,e.isBoolean=t,e.isNumber=i,e.isTrueNaN=o,e.isString=r,e.isFunction=u});
+define(["require","exports"],function(n,e){function t(n){return typeof n===N}function i(n,e){return void 0===e&&(e=!0),typeof n===c&&(e||!isNaN(n))}function o(n){return typeof n===c&&isNaN(n)}function r(n){return typeof n===p}function u(n){return typeof n===E}function f(n){return typeof n===s}var N=typeof!0,c="number",p="string",s=typeof{},y="object",a="undefined",E=typeof u;e.BOOLEAN=N,e.NUMBER=c,e.STRING=p,e.OBJECT=s,e.NULL=y,e.UNDEFINED=a,e.FUNCTION=E,e.isBoolean=t,e.isNumber=i,e.isTrueNaN=o,e.isString=r,e.isFunction=u,e.isObject=f});
 //# sourceMappingURL=Types.js.map
