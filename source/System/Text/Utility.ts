6302ca5671548a48fbb9788e8539eee613281fbb
electricessence <oren@electrified.net>
2015-10-25 00:24:09 -0700

    Added doc.

--- source/System/Text/Utility.ts
+++ source/System/Text/Utility.ts
@@ -11,6 +11,14 @@ export function escapeRegExp(source:string):string {
 	return source.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
+/**
+ * Can trim any character or set of characters from the ends of a string.
+ * Uses a Regex escapement to replace them with empty.
+ * @param source
+ * @param chars A string or array of characters desired to be trimmed.
+ * @param ignoreCase
+ * @returns {string}
+ */
 export function trim(source:string, chars?:string|string[],ignoreCase?:boolean):string
 {
 	if(chars) {
