378e6cc40915e24e2898c20b27fe9180891ecfcd
electricessence <oren@electrified.net>
2015-12-03 22:47:53 -0800

    Added IPromiseConstructor<T>

--- source/System/Promises/IPromise.d.ts
+++ source/System/Promises/IPromise.d.ts
@@ -1,16 +1,27 @@
 /*
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
- * Based Upon: https://github.com/kriskowal/q
+ * Based Upon: https://github.com/Microsoft/TypeScript/issues/1664
  */
 
 /**
  * Basic promise interface.
  * Included for ease of use with other classes.
+ * Will eventually be included in lib.d.ts.
  */
-interface IPromise<T> {
 
-	then<TNext>(
-		onFulfill?: (value: T) => TNext | IPromise<TNext>,
-		onReject?: (error: any) => TNext | IPromise<TNext>): IPromise<TNext>;
+
+interface IPromiseConstructor<T>
+{
+	new (
+		init:(
+			resolve:(value:T | IPromise<T>) => void,
+			reject:(reason:any) => void) => void): IPromise<T>;
+}
+
+interface IPromise<T>
+{
+	then<TResult>(
+		onFulfilled:(value:T) => TResult | IPromise<TResult>,
+		onRejected:(reason:any) => TResult | IPromise<TResult>): IPromise<TResult>;
 }
