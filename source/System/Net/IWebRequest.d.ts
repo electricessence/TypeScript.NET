32cde535d7525b952ab7666a4803b6c9c0d18f59
electricessence <oren@electrified.net>
2016-02-24 20:59:26 -0800

    Beginnings of interfaces for WebRequest.

---
+++ source/System/Net/IWebRequest.d.ts
@@ -0,0 +1,18 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
+ * Based on: https://msdn.microsoft.com/en-us/library/system.net.webrequest%28v=vs.110%29.aspx
+ */
+
+///<reference path="Security/AuthenticationLevel.d.ts"/>
+///<reference path="Cache/IRequestCachePolicy.d.ts"/>
+
+interface IWebRequest {
+
+	authenticationLevel:Authenticationlevel;
+	cachePolicy:IRequestCachePolicy;
+	connectionGroupName:string;
+
+	contentLength:number;
+	contentType:string;
+}
