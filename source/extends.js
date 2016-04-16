4cff266e845cf12570ab132a78f322c3f55bc33b
electricessence <oren@electrified.net>
2016-04-15 20:39:51 -0700

    Preparing for noEmitHelpers

---
+++ source/extends.js
@@ -0,0 +1,26 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
+ */
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    function default_1(d, b) {
+        for (var p in b)
+            if (b.hasOwnProperty(p))
+                d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null
+            ? Object.create(b)
+            : (__.prototype = b.prototype, new __());
+    }
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = default_1;
+});
+//# sourceMappingURL=extends.js.map
---
+++ source/extends.ts
@@ -0,0 +1,13 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
+ */
+
+export default function (d:any, b:any):void {
+	for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+	function __() { this.constructor = d; }
+	//noinspection CommaExpressionJS
+	d.prototype = b === null
+		? Object.create(b)
+		: (__.prototype = b.prototype, new (<any>__)());
+};
