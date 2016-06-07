656a715abdf8d69749e63ad83ca73aef2aeb53d2
electricessence <oren@electrified.net>
2016-06-07 01:53:22 -0700

    Eliminated compile risk.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,5 @@
-(function (factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define(["require", "exports", "./gulp/constants/Targets", "./gulp/constants/ModuleTypes", "./gulp/constants/TaskNames", "gulp", "./gulp/tsc"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    var TARGET = require("./gulp/constants/Targets");
-    var MODULE = require("./gulp/constants/ModuleTypes");
-    var TASK = require("./gulp/constants/TaskNames");
-    var gulp = require("gulp");
-    var tsc = require("./gulp/tsc");
-    gulp.task(TASK.SOURCE, function () { return tsc.atV2('./source', TARGET.ES5, MODULE.UMD, true); });
-    require("./gulp/tests");
-    require("./gulp/dist");
-    require("./gulp/bumpVersion");
-    require("./gulp/nuget-pack");
-    gulp.task(TASK.BUILD, [
-        TASK.SOURCE,
-        TASK.DIST,
-        TASK.BUILD + ".tests"
-    ]);
-    gulp.task(TASK.DEFAULT, [
-        TASK.SOURCE,
-        TASK.DIST
-    ]);
-});
+require("./gulp/tests");
+require("./gulp/dist");
+require("./gulp/bumpVersion");
+require("./gulp/nuget-pack");
 //# sourceMappingURL=gulpfile.js.map
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,31 +1,33 @@
-import * as TARGET from "./gulp/constants/Targets";
-import * as MODULE from "./gulp/constants/ModuleTypes";
-import * as TASK from "./gulp/constants/TaskNames";
-import * as gulp from "gulp";
-import * as tsc from "./gulp/tsc";
+// import * as TARGET from "./gulp/constants/Targets";
+// import * as MODULE from "./gulp/constants/ModuleTypes";
+// import * as TASK from "./gulp/constants/TaskNames";
+// import * as gulp from "gulp";
+// import * as tsc from "./gulp/tsc";
 
-
-gulp.task(
-	// This should render the same output as WebStorm's configuration.
-	TASK.SOURCE,
-	()=> tsc.atV2('./source', TARGET.ES5, MODULE.UMD, true)
-);
+// This is currently disabled since the output cannot be matched exactly.
+// The source files must be build by tsc.exe or WebStorm's built-in tsc.
+// =============
+// gulp.task(
+// 	// This should render the same output as WebStorm's configuration.
+// 	TASK.SOURCE,
+// 	()=> tsc.at('./source', TARGET.ES5, MODULE.UMD)
+// );
 
 require("./gulp/tests");
 require("./gulp/dist");
 require("./gulp/bumpVersion");
 require("./gulp/nuget-pack");
 
-gulp.task(
-	TASK.BUILD, [
-		TASK.SOURCE,
-		TASK.DIST,
-		TASK.BUILD + ".tests"
-	]
-);
+// gulp.task(
+// 	TASK.BUILD, [
+// 		TASK.SOURCE,
+// 		TASK.DIST,
+// 		TASK.BUILD + ".tests"
+// 	]
+// );
 
 
-gulp.task(TASK.DEFAULT, [
-	TASK.SOURCE,
-	TASK.DIST
-]);
+// gulp.task(TASK.DEFAULT, [
+// 	TASK.SOURCE,
+// 	TASK.DIST
+// ]);
