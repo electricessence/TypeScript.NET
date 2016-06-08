b27b9b0acfedf0fec2d187a157a33e5f5d08cc65
electricessence <oren@electrified.net>
2016-06-08 14:58:02 -0700

    Update Parallel.ts

--- source/System/Threading/Tasks/Parallel.ts
+++ source/System/Threading/Tasks/Parallel.ts
@@ -1,7 +1,7 @@
 /*!
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT https://github.com/electricessence/TypeScript.NET/blob/master/LICENSE.md
- * Based upon Parallel.js: https://github.com/adambom/parallel.js/blob/master/lib/parallel.js
+ * Originally based upon Parallel.js: https://github.com/adambom/parallel.js/blob/master/lib/parallel.js
  */
 
 import {Promise, PromiseBase, Fulfilled} from "../../Promises/Promise";
