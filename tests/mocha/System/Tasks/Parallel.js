baa4d2bde0f6c126ab7621c2e099a4d5239ce2a4
Oren (electricessence) <oren@electrified.net>
2016-11-02 06:09:38 -0700

    Improved type inference with strict null checks enabled. Updated mocha tests to ensure compatibility with strict null checks. Removed unneeded types="assert" reference since it wasn't really causing a problem.

--- tests/mocha/System/Tasks/Parallel.js
+++ tests/mocha/System/Tasks/Parallel.js
@@ -1,11 +1,1 @@
-(function (factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-});
 //# sourceMappingURL=Parallel.js.map
